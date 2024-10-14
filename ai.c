5acd38babd068c447d867988f6259ab9642e1df3
Dereck Portela <dereck_alexander@outlook.com>
2024-10-14 20:51:56 -0300

    add ai logic

--- ai.c
+++ ai.c
@@ -1,19 +1,112 @@
 #include <stdio.h>
 #include "board.h"
 #include "player.h"
+#include "menus.h"
+#include "game.h"
 #include "ai.h"
 
-void aiMove(int *player){
-    int mark = 1;
-    for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-            if(board[i][j] == ' '){
-                board[i][j] = PLAYER_2;
-                mark = 0;
-                break;
+int minMaxWithDepth(int isMaximizing, int depth)
+{
+    int score = 0;
+
+    if (depth == 0)
+        return 0;
+
+    score = checkWinner();
+    if (score != 0)
+        return score;
+
+    if (!checkEmptySpaces())
+        return 0; // Tie
+
+    if (isMaximizing)
+    {
+
+        int bestScore = 1000;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i][j] == ' ')
+                {
+                    board[i][j] = PLAYER_2;
+                    int score = minMaxWithDepth(0, depth - 1);
+                    board[i][j] = ' ';
+                    bestScore = (score < bestScore) ? score : bestScore;
+                }
             }
-            
         }
-        if(!mark) break;
+
+        return bestScore;
     }
+    else
+    {
+        int bestScore = -1000, score = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i][j] == ' ')
+                {
+                    board[i][j] = PLAYER_1;
+                    score = minMaxWithDepth(1, depth - 1);
+                    board[i][j] = ' ';
+                    bestScore = (score > bestScore) ? score : bestScore;
+                }
+            }
+        }
+
+        return bestScore;
+    }
+}
+
+void aiMove(int *player)
+{
+    printf("Ai move!!!\n");
+    int bestScore = 1000, bestRow = -1, bestCol = -1, depth = aiLevel * 1000, score;
+
+    switch (aiLevel)
+    {
+    case 1:
+        depth = 1;
+        break;
+    case 2:
+        depth = 2;
+        break;
+    case 3:
+        depth = 4;
+        break;
+    case 4:
+        depth = 100000;
+        break;
+        
+    
+    default:
+        break;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] == ' ')
+            {
+                board[i][j] = PLAYER_2;
+                score = minMaxWithDepth(0, depth - 1);
+                board[i][j] = ' ';
+
+                if (score < bestScore)
+                {
+                    bestScore = score;
+                    bestRow = i;
+                    bestCol = j;
+                }
+            }
+        }
+    }
+
+    board[bestRow][bestCol] = PLAYER_2;
+
+    printf("Ai chooses row: %d and col: %d\n\n", bestRow, bestCol);
 }
--- board.c
+++ board.c
@@ -11,7 +11,7 @@ void printBoard() {
   printf("\n---|---|---\n");
   printf(" %c | %c | %c", board[2][0], board[2][1], board[2][2]);
   printf("\n---|---|---\n");
-  printf("\n");
+  printf("\n\n\n");
 }
 
 int checkEmptySpaces() {
